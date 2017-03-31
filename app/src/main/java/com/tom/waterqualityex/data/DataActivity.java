c82ae8e6d394f85a7b0d0f73f1cf81539611c012
Olive Tom <mengxinchn@gmail.com>
2017-03-31 22:01:41 +0800

    data完成一部分

---
+++ app/src/main/java/com/tom/waterqualityex/data/DataActivity.java
@@ -0,0 +1,15 @@
+package com.tom.waterqualityex.data;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tom.waterqualityex.R;
+
+public class DataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+    }
+}
