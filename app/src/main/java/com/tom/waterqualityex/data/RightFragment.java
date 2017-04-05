90c39b5939a231e9e19b8015b7899f90ee8417d1
Olive Tom <mengxinchn@gmail.com>
2017-04-05 19:41:16 +0800

    天气集成成功

--- app/src/main/java/com/tom/waterqualityex/data/RightFragment.java
+++ app/src/main/java/com/tom/waterqualityex/data/RightFragment.java
@@ -1,15 +1,36 @@
 package com.tom.waterqualityex.data;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.tom.waterqualityex.R;
+import com.tom.waterqualityex.domain.gson.Weather;
+import com.tom.waterqualityex.global.GlobalConstants;
+import com.tom.waterqualityex.utils.Engine;
+import com.tom.waterqualityex.utils.HttpUtil;
+import com.tom.waterqualityex.utils.SpUtil;
+import com.tom.waterqualityex.utils.ToastUtil;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 
 public class RightFragment extends Fragment {
+
+    private String mProvince;
+    private String mCity;
+    private String mCounty;
+    private View mRightView;
+    private TextView aqi_zhishu;
+    private TextView pm_zhishu;
 
     public RightFragment() {
 
@@ -18,6 +39,82 @@ public class RightFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_right, container, false);
+        mRightView = inflater.inflate(R.layout.fragment_right, container, false);
+        initUI();
+        getLocalWeatherInfo();
+        return mRightView;
+    }
+
+    private void getLocalWeatherInfo() {
+        //获取当地天气信息
+        String weatherString = SpUtil.getString(getActivity(), GlobalConstants.WEATHER, null);
+        if(weatherString != null){
+            //有缓存直接解析天气数据
+            Weather weather = Engine.handleWeatherResponse(weatherString);
+            showWeatherInfo(weather);
+        }else{
+            //获取当地天气信息
+            getLocalWeather();
+        }
+
+    }
+
+    private void initUI() {
+        aqi_zhishu = (TextView) mRightView.findViewById(R.id.aqi_zhishu);
+        pm_zhishu = (TextView) mRightView.findViewById(R.id.pm_zhishu);
+    }
+
+    /**
+     * 获取当地天气信息
+     */
+    private void getLocalWeather() {
+        mProvince = SpUtil.getString(getActivity(), GlobalConstants.PROVINCE, "浙江省");
+        mCity = SpUtil.getString(getActivity(), GlobalConstants.CITY, "杭州市");
+        mCounty = SpUtil.getString(getActivity(), GlobalConstants.DISTRICT, "临安");
+        Intent intent = new Intent(getActivity(), AutoUpdateService.class);
+        getActivity().startService(intent);
+        Engine.findWeatherURL(mProvince, mCity, mCounty, new Engine.WeatherIdFinded() {
+            @Override
+            public void success(String weatherId) {
+                String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=a615da100d8e4631a3e80367a5c046f8";
+                HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                ToastUtil.show(getActivity(), "获取天气信息失败");
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        final String responseText = response.body().string();
+                        final Weather weather = Engine.handleWeatherResponse(responseText);
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(weather != null && "ok".equals(weather.status)){
+                                    SpUtil.setString(getActivity(), GlobalConstants.WEATHER, responseText);
+                                    showWeatherInfo(weather);
+                                }else{
+                                    ToastUtil.show(getActivity(), "获取天气信息失败");
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * 处理接受到的天气数据
+     * @param weather
+     */
+    private void showWeatherInfo(Weather weather) {
+        aqi_zhishu.setText(weather.aqi.city.aqi);
+        pm_zhishu.setText(weather.aqi.city.pm25);
     }
 }
---
+++ app/src/main/java/com/tom/waterqualityex/global/GlobalConstants.java
@@ -0,0 +1,24 @@
+package com.tom.waterqualityex.global;
+
+/**
+ * Created by mengxin on 16-9-23.
+ */
+public class GlobalConstants {
+    public static final String CONFIG = "config";
+    public static final String IS_FIRST_ENTER = "is_first_enter";
+    /**
+     * 站点ｕｒｌ
+     */
+    public static final String SITE_URL = "http://115.236.84.154/12345.csv";
+    public static final String PROVINCE = "province";
+    public static final String CITY = "city";
+    public static final String DISTRICT = "district";
+    public static final String PROVINCE_URL = "http://guolin.tech/api/china";
+    public static final String WEATHER = "weather";
+
+    /**
+     * 站点Url
+     */
+    public static final String SITE_BASE_URL = "http://115.236.84.154/";
+
+}
