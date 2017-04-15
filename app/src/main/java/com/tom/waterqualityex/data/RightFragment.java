ed6b3f84d97fb181b74148f6cf34c7543a21b8bd
Olive Tom <mengxinchn@gmail.com>
2017-04-15 18:34:23 +0800

    cun

--- app/src/main/java/com/tom/waterqualityex/data/RightFragment.java
+++ app/src/main/java/com/tom/waterqualityex/data/RightFragment.java
@@ -1,22 +1,40 @@
 package com.tom.waterqualityex.data;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.tom.waterqualityex.R;
 import com.tom.waterqualityex.domain.gson.Weather;
 import com.tom.waterqualityex.global.GlobalConstants;
+import com.tom.waterqualityex.http.entity.WaterData;
+import com.tom.waterqualityex.http.http.HttpMethods;
+import com.tom.waterqualityex.http.subscribers.SimpleHttpSubscriber;
+import com.tom.waterqualityex.http.subscribers.SubscriberOnNextListener;
 import com.tom.waterqualityex.utils.Engine;
 import com.tom.waterqualityex.utils.HttpUtil;
 import com.tom.waterqualityex.utils.SpUtil;
 import com.tom.waterqualityex.utils.ToastUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -25,6 +43,7 @@ import okhttp3.Response;
 
 public class RightFragment extends Fragment {
 
+    private static final int DELAYED_MESSAGE = 1;
     private String mProvince;
     private String mCity;
     private String mCounty;
@@ -34,6 +53,35 @@ public class RightFragment extends Fragment {
     private TextView shushidu;
     private TextView xichezhishu;
     private TextView yunxingjianyi;
+    private LineChart mChart;
+    private TextView ph;
+    private TextView shuiwen;
+    private TextView diandaolv;
+    private TextView rongjieyang;
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what){
+                case DELAYED_MESSAGE:
+
+                    //获取一次天气信息
+                    getLocalWeatherInfo();
+                    //获取一次图表信息
+                    fillTheChart();
+                    //获取一次常规参数信息
+                    fillCommonInfo();
+                    mHandler.sendEmptyMessageDelayed(DELAYED_MESSAGE, DelayedTime);
+                    break;
+                default:
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+    private long DelayedTime = 1000 * 60 * 5;
+    private TextView tianqi;
+    private TextView wendu;
 
     public RightFragment() {
 
@@ -44,8 +92,100 @@ public class RightFragment extends Fragment {
                              Bundle savedInstanceState) {
         mRightView = inflater.inflate(R.layout.fragment_right, container, false);
         initUI();
+        //获取一次天气信息
         getLocalWeatherInfo();
+        //获取一次图表信息
+        fillTheChart();
+        //获取一次常规参数信息
+        fillCommonInfo();
+
+        mHandler.sendEmptyMessageDelayed(DELAYED_MESSAGE, DelayedTime);
         return mRightView;
+    }
+
+    private void fillCommonInfo() {
+        HttpMethods.getInstance().getWaterData(new SimpleHttpSubscriber<List<WaterData>>(new SubscriberOnNextListener<List<WaterData>>() {
+            @Override
+            public void onNext(List<WaterData> waterDatas) {
+                WaterData waterData = waterDatas.get(waterDatas.size() - 1);
+                shuiwen.setText(waterData.getShuiWen() + "");
+                diandaolv.setText(waterData.getDianDaoLv() + "");
+                ph.setText(waterData.getPh() + "");
+                rongjieyang.setText(waterData.getRongJieYang() + "");
+            }
+        }), "D01", "2017-04-12");
+    }
+
+
+    /**
+     * 填充图表
+     */
+    private void fillTheChart() {
+        HttpMethods.getInstance().getWaterData(new SimpleHttpSubscriber<List<WaterData>>(new SubscriberOnNextListener<List<WaterData>>() {
+            @Override
+            public void onNext(List<WaterData> waterDatas) {
+                System.out.println("123123" + waterDatas);
+                List<Entry> entries = new ArrayList<Entry>();
+                for (WaterData w :
+                        waterDatas) {
+                    String t = w.getTime();
+                    String[] sp = t.split(" ");
+
+                    String[] split = sp[1].split(":");
+                    float time = Float.parseFloat(split[0]) * 60 + Float.parseFloat(split[1]);
+                    float anDan = w.getAnDan();
+                    entries.add(new Entry(time, anDan));
+                }
+
+                LineDataSet dataSet = new LineDataSet(entries, "非常规参数--氨氮"); // add entries to dataset
+                dataSet.setColor(Color.BLUE);
+                dataSet.setValueTextColor(Color.WHITE); // styling, ...
+                LineData lineData = new LineData(dataSet);
+                mChart.setData(lineData);
+                //设置描述信息
+                Description mChartDescription = new Description();
+//                mChartDescription.setText("非常规参数--氨氮");
+//                mChartDescription.setTextSize(15);
+//                mChartDescription.setTextColor(mActivity.getResources().getColor(R.color.textColor));
+                mChartDescription.setEnabled(false);
+                mChart.setDescription(mChartDescription);
+
+
+                //设置x轴坐标
+                XAxis xAxis = mChart.getXAxis();
+                xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                xAxis.setDrawAxisLine(true);
+                xAxis.setDrawGridLines(true);
+                xAxis.setDrawAxisLine(true);
+                xAxis.setEnabled(true);
+                xAxis.setAxisLineWidth(2f);
+                xAxis.setTextColor(getActivity().getResources().getColor(R.color.textColor));
+                xAxis.setLabelCount(7);
+                // the labels that should be drawn on the XAxis
+                final String[] quarters = new String[] { "0h", "1h", "2h", "3h",
+                        "4h", "5h", "6h", "7h", "8h", "9h", "10h", "11h", "12h", "13h",
+                        "14h", "15h", "16h", "17h", "18h",
+                        "19h", "20h", "21h", "22h", "23h", "24h"};
+                IAxisValueFormatter formatter = new IAxisValueFormatter() {
+                    @Override
+                    public String getFormattedValue(float value, AxisBase axis) {
+                        return quarters[(int) value / 60];
+                    }
+                };
+                xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+                xAxis.setValueFormatter(formatter);
+
+                //设置Y轴属性
+                YAxis leftAxis = mChart.getAxisLeft();
+                leftAxis.setAxisLineWidth(2f);
+                leftAxis.setTextColor(getActivity().getResources().getColor(R.color.textColor));
+                YAxis rightAxis = mChart.getAxisRight();
+                rightAxis.setAxisLineWidth(2f);
+                rightAxis.setTextColor(getActivity().getResources().getColor(R.color.textColor));
+
+                mChart.invalidate(); // refresh
+            }
+        }), "D01", "2017-04-12");
     }
 
     private void getLocalWeatherInfo() {
@@ -68,6 +208,15 @@ public class RightFragment extends Fragment {
         shushidu = (TextView) mRightView.findViewById(R.id.shushidumiaoshu);
         xichezhishu = (TextView) mRightView.findViewById(R.id.xichezhishumiaoshu);
         yunxingjianyi = (TextView) mRightView.findViewById(R.id.yunxingjianyimiaoshu);
+
+        ph = (TextView) mRightView.findViewById(R.id.phzhi);
+        shuiwen = (TextView) mRightView.findViewById(R.id.shuiwenzhi);
+        diandaolv = (TextView) mRightView.findViewById(R.id.diandaolvzhi);
+        rongjieyang = (TextView) mRightView.findViewById(R.id.rongjieyangzhi);
+        mChart = (LineChart) mRightView.findViewById(R.id.chart);
+
+        tianqi = (TextView) mRightView.findViewById(R.id.tianqi);
+        wendu = (TextView) mRightView.findViewById(R.id.wendu);
     }
 
     /**
@@ -127,5 +276,8 @@ public class RightFragment extends Fragment {
         shushidu.setText(weather.suggestion.comfort.info);
         xichezhishu.setText(weather.suggestion.carWash.info);
         yunxingjianyi.setText(weather.suggestion.sport.info);
+
+        wendu.setText(weather.now.temperature + "℃");
+        tianqi.setText(weather.now.more.info);
     }
 }
